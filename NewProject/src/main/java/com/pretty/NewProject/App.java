aac9963d2f29989a62b3987c6db5f9acd62c39a0
Kalimah Locklin <knlocklin@yahoo.com>
2024-03-25 16:10:06 -0500

    first commit

---
+++ NewProject/src/main/java/com/pretty/NewProject/App.java
@@ -0,0 +1,13 @@
+package com.pretty.NewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
